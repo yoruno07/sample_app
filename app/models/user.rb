f1f6f541d5dede379466ba87f36c2cb41645e760
yoruno07 <ryo.yoru.io@gmail.com>
2018-02-06 23:18:58 +0900

    add user feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,6 +69,10 @@ end
     reset_sent_at < 2.hours.ago
   end
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
   # メールアドレスをすべて小文字にする
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
 
   def setup
-    @user = users (:michael)
+    @user = users(:michael)
     @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
